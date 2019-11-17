d6938954cb647048de77dc1584bcd165101b0f97
Dem-Ivan <demidenko.vanya@list.ru>
2019-11-17 17:27:30 +0300

    addclassWorcWithWolfram

---
+++ WebApi/WorcWihtWolfram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wolfram.NETLink;
+
+namespace WebApi
+{
+	public class WorcWihtWolfram
+	{
+		MathKernel mathKernel = new MathKernel();
+		public string SimplifyExpression(string expression)
+		{
+			mathKernel.Compute("Simplify[" + expression + "]");
+			return mathKernel.Result.ToString();
+		}
+	}
+}
